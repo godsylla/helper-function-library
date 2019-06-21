837e67fa131e980879f141e325c77f7635e4eba1
Sylvia Tran <sylvia.tran@nielsen.com>
2019-06-20 18:34:32 -0700

    Update athena_helper.py

--- aws_helper_functions/athena_helper.py
+++ aws_helper_functions/athena_helper.py
@@ -4,57 +4,46 @@ import io
 import time
 import pandas as pd 
 
-s3_client = boto3.client('s3', region_name='us-west-2')
-s3_resource = boto3.resource('s3', region_name='us-west-2')
 
-athena_client = boto3.client('athena', region_name='us-west-2')
-
-
-# Function to query Athena
-def run_query (query:str, database:str, s3_output_location:str):
-    assert 's3://' in s3_output_location, "s3_output_location must have prefix s3://"
-
-    response = athena_client.start_query_execution(
-        QueryString = query,
-        QueryExecutionContext = {
-            'Database': database
-        },
-        ResultConfiguration = {
-            'OutputLocation': s3_output_location
-        }
-    )
-
-    execution_id = response['QueryExecutionId']
-    return execution_id, response
+def execute_athena_query (query_string:str, database_name:str, s3_output_location:str) -> dict:
+    assert 's3://' in s3_output_location, "s3_output_locaton requires the prefix 's3://'."
+    
+    athena_client = boto3.client('athena')
+    
+    response = athena_client.start_query_execution(QueryString=query_string,
+                                                   QueryExecutionContext={
+                                                       'Database' : database_name
+                                                   },
+                                                   ResultConfiguration={
+                                                       'OutputLocation': s3_output_location
+                                                   })
+    return response
 
 
-# Execute query
-def execute_query (query:str, database:str, s3_output_location:str):
+def query_results_to_csv_to_df (athena_query_response:dict, bucket_name:str, **s3_key:str) -> pd.DataFrame:
 
-    execution_id, _ = run_query(query=query, database=database, s3_output_location=s3_output_location)
-
-    while True:
-        stats = athena_client.get_query_execution(QueryExecutionId=execution_id)
-        status = stats['QueryExecution']['Status']['State']
-        print(status)
-
-        if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
-            break
-        time.sleep(10)
-
-
-# Retrieve results written to S3 output location and return a pandas DataFrame
-def return_query_results_as_df (bucket_name:str, query_execution_id:str):
-    obj = s3_client.get_object(Bucket=bucket_name, Key=f'{query_execution_id}.csv')
-
-    return pd.read_csv(io.BytesIO(obj['Body'].read()))
-
+    # make function ambiv to if the s3:// prefix is in the argument
+    if bucket_name[:5] == 's3://':
+        bucket_name = bucket_name[5:]
+    else:
+        bucket_name
         
-# Function to clear the S3 bucket after the queries are run
-def clean_bucket (bucket_name: str):
-    assert 's3://' not in bucket_name, 'The s3:// prefix is not needed in the bucket name.'
-    my_bucket = s3_resource.Bucket(bucket_name)
-    my_bucket.objects.all().delete()
+    s3_client = boto3.client('s3')
+    
+    query_exec_id = athena_query_response['QueryExecutionId'] 
+    filename = query_exec_id + '.csv'
+    
+    key = s3_key.get('s3_key', '')
+    if key[-1] == '/':
+        key_path = key + filename
+    else:
+        key_path = key + '/' + filename
+    
+    obj = s3_client.get_object(Bucket=bucket_name,
+                              Key=key_path)
+    
+    df = pd.read_csv(io.BytesIO(obj['Body'].read()))
+    return df
 
 
 # TODO: Write test code for the various functions (instead of stashing it all into the main() function)

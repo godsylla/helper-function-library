65f5d066aa4968f5ec4f5b84f492e8caab3aadcc
Sylvia Tran <sylvi.tran@gmail.com>
2019-07-23 11:06:10 -0700

    Update athena_helper.py

--- aws_helper_functions/athena_helper.py
+++ aws_helper_functions/athena_helper.py
@@ -3,9 +3,15 @@ import botocore
 import io
 import time
 import pandas as pd 
+import random
+
+s3_client = boto3.client('s3', region_name='us-west-2')
+s3_resource = boto3.resource('s3', region_name='us-west-2')
+
+athena_client = boto3.client('athena', region_name='us-west-2')
 
 
-def execute_athena_query (query_string:str, database_name:str, s3_output_location:str) -> dict:
+def execute_athena_query (query_string:str, database_name:str, s3_output_location:str, random_sleep_bounds=(4,8)) -> dict:
     assert 's3://' in s3_output_location, "s3_output_locaton requires the prefix 's3://'."
     
     athena_client = boto3.client('athena')
@@ -17,7 +23,19 @@ def execute_athena_query (query_string:str, database_name:str, s3_output_locatio
                                                    ResultConfiguration={
                                                        'OutputLocation': s3_output_location
                                                    })
-    return response
+    execution_id = response['QueryExecutionId']
+    
+    while True:
+        res = athena_client.get_query_execution(QueryExecutionId=execution_id)
+        status = res['QueryExecution']['Status']['State']
+        if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+            if status == 'SUCCEEDED':
+                return res
+            else:
+                raise ValueError(res)
+        time.sleep(random.uniform(*random_sleep_bounds))
+
+    # return response
 
 
 def query_results_to_csv_to_df (athena_query_response:dict, bucket_name:str, **s3_key:str) -> pd.DataFrame:
@@ -30,7 +48,7 @@ def query_results_to_csv_to_df (athena_query_response:dict, bucket_name:str, **s
         
     s3_client = boto3.client('s3')
     
-    query_exec_id = athena_query_response['QueryExecutionId'] 
+    query_exec_id = athena_query_response['QueryExecution']['QueryExecutionId'] 
     filename = query_exec_id + '.csv'
     
     key = s3_key.get('s3_key', '')
@@ -44,6 +62,5 @@ def query_results_to_csv_to_df (athena_query_response:dict, bucket_name:str, **s
     
     df = pd.read_csv(io.BytesIO(obj['Body'].read()))
     return df
-
-
-# TODO: Write test code for the various functions (instead of stashing it all into the main() function)
+     
+# # TODO: Write test code for the various functions (instead of stashing it all into the main() function)
